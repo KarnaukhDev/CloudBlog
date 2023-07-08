c58e30ea42dbc0f6fe10ef95a13c1e655bedc40a
karnaukhdev <karnaukhdev@gmail.com>
2023-07-08 18:09:23 +0300

    fix: single post page + link to post

--- src/components/PostPage.tsx
+++ src/components/PostPage.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
+import * as source from '../data/posts.json'
 
+const data = {source};
+const posts = data.source.posts;
+
+type QuizParams = {
+    id: string;
+};
+type InitialPost = {
+    id: number,
+    title: string,
+    body: string,
+    date: string,
+    images: string[],
+    preview: string
+};
 const PostPage = () => {
+    const {id} = useParams<keyof QuizParams>() as QuizParams;
+
+    const currentPost: InitialPost = posts.find(p => p.id === parseInt(id, 10)) || {} as InitialPost;
+
     return (
-        <div className='posts-page'>
-            <h1>POST 123 PAGE</h1>
+        <div className='post-page'>
+            <div className='post'>
+                <p className='post-title'>{currentPost?.title}</p>
+                <div>
+                    <p className='post-body'>
+                        {currentPost?.body}
+                    </p>
+                </div>
+                <div className='images-block'>
+                    <div className='images-preview'>
+                        {currentPost.images.map((image, imageIndex) => {
+                            return (
+                                <img src={image} alt="" className='preview-image' key={imageIndex}/>
+                            );
+                        })}
+                    </div>
+                    <div className='additional-info'>
+                        <p className='post-date'>{currentPost.date}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/PostsPage.tsx
+++ src/components/PostsPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import * as source from '../data/posts.json';
+import { Link } from "react-router-dom";
+import * as source from '../data/posts.json'
 
 const data = {source};
 const posts = data.source.posts;
@@ -11,12 +12,18 @@ const PostsPage = () => {
                 return (
                     <div className='post' key={index}>
                         <p className='post-title'>{post.title}</p>
-                        <p className='post-body'>{post.body}</p>
+                        <div>
+                            <p className='post-body'>
+                                {(post.body.length > 500) ? post.body.slice(0, 499) + '…' : post.body}
+                                <Link to={ `/posts/${post.id}` }><span className='more'>read more</span></Link>
+                            </p>
+                        </div>
+
                         <div className='images-block'>
                             <div className='images-preview'>
                                 {post.images.map((image, imageIndex) => {
                                     return (
-                                        <img src={image} alt="" className='preview-image'/>
+                                        <img src={image} alt="" className='preview-image' key={imageIndex}/>
                                     );
                                 })}
                             </div>
@@ -24,7 +31,6 @@ const PostsPage = () => {
                                 <p className='post-date'>{post.date}</p>
                             </div>
                         </div>
-
                     </div>
                 );
             })}
